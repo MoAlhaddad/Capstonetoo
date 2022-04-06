70d8000390679c79a08b79ecfbf823f7233471eb
Ali Qassem Alhaddad <52386724+MoAlhaddad@users.noreply.github.com>
2022-04-05 20:53:30 -0400

    Use Build Folder on Frontend
    Use build folder on frontend.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const path = require('path');
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -11,6 +12,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+ app.get('/', (req, res) => {
+   res.redirect('/app/');
+ });
+
+app.get('/app/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 app.use("/api/jobs", require("./routes/jobRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
