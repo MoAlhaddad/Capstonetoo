56b92f2d24e6bd77da3f1d750c7cbe742153373f
Ali Qassem Alhaddad <52386724+MoAlhaddad@users.noreply.github.com>
2022-04-06 00:21:48 -0400

    attempt 11
    attempt 11

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, '.  /build')));
 //Server frontend
 if(process.env.NODE_ENV === 'production') {
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         console.log(path.resolve(__dirname,'./build', 'index.html'));
         res.sendFile(path.resolve(__dirname,'./build', 'index.html'))
     })
