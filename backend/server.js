ba02243d3296cb7c53cde2288477cdb3a06c738d
Ali Qassem Alhaddad <52386724+MoAlhaddad@users.noreply.github.com>
2022-04-05 23:51:13 -0400

    attempt 7
    attempt 7

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/build')));
 //Server frontend
 if(process.env.NODE_ENV === 'production') {
-    app.get("/", (req, res) => res.redirect("/app/"));
-    app.get('/app/*', (req, res) => res.sendFile(path.resolve(__dirname, '/build', 'index.html')))
+    app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, '/build', 'index.html')))
 }
 
 app.use("/api/jobs", require("./routes/jobRoutes"));
