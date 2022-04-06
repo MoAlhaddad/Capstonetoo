4ab71cc32a8a940cb8460e0402164903caa84331
Ali Qassem Alhaddad <52386724+MoAlhaddad@users.noreply.github.com>
2022-04-05 21:42:48 -0400

    Try to fix heroku issues 3
    Try to fix heroku issues 3

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-//Server frontend
-if(process.env.NODE_ENV === 'production') {
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html')))
-}
-
 
 app.use("/api/jobs", require("./routes/jobRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
@@ -27,3 +21,5 @@ app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports = { app };
